bfc1d4292434b94571c171ddf9f08e3c3bd622f2
ArtemMikhailenko <Artemlend@gmail.com>
2026-05-06 16:10:05 +0300

    feat: add Google Analytics GA4 (G-RXQS8GDG2B) and Yandex Metrika (104388590)

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,6 +87,30 @@ export default async function RootLayout({
           }}
         />
 
+        {/* Google Analytics GA4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RXQS8GDG2B" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RXQS8GDG2B');`,
+          }}
+        />
+
+        {/* Yandex Metrika */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+            ym(104388590,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',accurateTrackBounce:true,trackLinks:true});`,
+          }}
+        />
+
         {/* Preconnect for performance */}
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
@@ -124,6 +148,12 @@ export default async function RootLayout({
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           />
+        </noscript>
+        {/* Yandex Metrika (noscript) */}
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/104388590" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
         </noscript>
         {children}
       </body>
