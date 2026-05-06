db934cff44c91100dce34acf07e95229bfdc56c8
ArtemMikhailenko <Artemlend@gmail.com>
2026-05-06 16:12:52 +0300

    fix: Yandex Metrika URL with correct ID param + referrer/url tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,10 +106,12 @@ export default async function RootLayout({
               m[i].l=1*new Date();
               for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
-            ym(104388590,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',accurateTrackBounce:true,trackLinks:true});`,
+            })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=104388590','ym');
+            ym(104388590,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
           }}
         />
+
+
 
         {/* Preconnect for performance */}
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
